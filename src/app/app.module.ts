13fdcdc189dbaa9cf149ee6252ec67e0bb4c06d0
Paola Arrueé <paolaarruee@gmail.com>
2021-12-27 18:13:00 -0300

    Criando component para Menu do Admin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ConfigAdminComponent } from './config-admin/config-admin.component';
+import { MenuDoAdminComponent } from './menu-do-admin/menu-do-admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConfigAdminComponent,
+    MenuDoAdminComponent
   ],
   imports: [
     BrowserModule,

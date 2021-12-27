e2da4176bb4a5ee13a4245ba5ab7b7040f413f89
felipebonadio <febonadio@gmail.com>
2021-12-27 16:50:59 -0300

    Criado o modal que exibe o alerta

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ModalService } from './service/modal.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'ProjetoLilaFront';
+  constructor(private modalService: ModalService) { }
+
+  ngOnInit(): void {    
+  }
+
+  abrirModal(id: string) {
+    this.modalService.abrir(id);
+  }
+
+  fecharModal(id: string) {
+    this.modalService.fechar(id);
+  }
+
+  validar(){
+    let validar = true;
+    if( validar == true){
+      this.abrirModal('mensagemErro');
+    }
+  }
 }

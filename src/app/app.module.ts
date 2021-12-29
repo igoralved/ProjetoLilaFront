75b479e1f8f38c7e4cf838546ad9c6fe827fd767
Paola Arrueé <paolaarruee@gmail.com>
2021-12-29 14:01:37 -0300

    botões e logo do menu de cadastro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+import { MatRippleModule } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ConfigAdminComponent } from './config-admin/config-admin.component';
 import { MenuDoAdminComponent } from './menu-do-admin/menu-do-admin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ConfigAdminComponent,
     MenuDoAdminComponent
   ],
   imports: [
     BrowserModule,
+    MatIconModule,
+    MatButtonModule,
+    MatRippleModule,
     AppRoutingModule,
     BrowserAnimationsModule
   ],

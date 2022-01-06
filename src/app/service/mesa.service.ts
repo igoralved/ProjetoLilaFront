70e33c11e34e30ebac41c9a93303f2c5418c383d
Gabriel Lima <gabriellimab@gmail.com>
2022-01-06 16:04:22 -0300

    Configurada a conexão websocket.

--- src/app/service/mesa.service.ts
+++ src/app/service/mesa.service.ts
@@ -4,6 +4,9 @@ import { Router } from '@angular/router';
 import { WebSocketConnector } from 'src/websocket/websocket-connector';
 import { environment } from 'src/environments/environment';
 import { Sala } from 'src/app/model/sala';
+import { Jogador } from '../model/jogador';
+import { Observable } from 'rxjs';
+import { SalaRequest } from '../model/salaRequest';
 
 @Injectable({
   providedIn: 'root',
@@ -16,26 +19,38 @@ export class MesaService {
     this.webSocketConnector = {} as WebSocketConnector;
   }
 
-  connect(id: string) {
-    this.webSocketConnector = new WebSocketConnector(
-      //TODO: pegar a url correta com a dupla do back
-      environment.API_URL,
-      '/statusProcessor' + id,
-      this.onMessage.bind(this)
-    );
-    
-  }
-  //TODO: verificar com a dupla do back a url correta
-  start(sala: Sala) {
-    this.http
-      .put<Sala>(`${environment.API_URL}/api`, sala)
-      .subscribe((response) => console.log(response));
+  /* Envia a informação do primeiro jogador para o back end.
+   * Recebe as informações da sala criada.
+   */
+  iniciarHost(jogador: Jogador): Observable<Sala> {
+    return this.http.post<Sala>(`${environment.API_URL}api/iniciar`, jogador);
   }
 
+  /* O novo jogador se conecta através desta requisição
+   *
+   */
+  conectarNovoJogador(salaRequest: SalaRequest): Observable<Sala> {
+    return this.http.post<Sala>(
+      `${environment.API_URL}/api/conectar`,
+      salaRequest
+    );
+  }
+
+  connectWebsocket(hash: string) {
+    this.webSocketConnector = new WebSocketConnector(
+      environment.API_URL,
+      `/topic/${hash}`,
+      //TODO: Linha 43 precisa ser modificada de acordo com a lógica das jogadas
+      this.onMessage.bind(this)
+    );
+  }
+
+  /* TODO: lógica das jogadas */
   onMessage(message: any): void {
     this.items.push(message.body);
   }
 
+  /* TODO: lógica das jogadas */
   mostrarMensagem(): any {
     return this.items;
   }

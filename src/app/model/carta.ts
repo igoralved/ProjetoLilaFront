b96d7309d4f138aa4d73c9bc5d9961d76e00c51d
Paola Arrueé <paolaarruee@gmail.com>
2022-01-06 15:01:05 -0300

    adicionando interface da carta

---
+++ src/app/model/carta.ts
@@ -0,0 +1,10 @@
+export interface carta {
+    tipo: string;
+    categoria: string;
+    bonus: boolean;
+    texto: string;
+    valorCorPequeno: number;
+    valorCorGrande: number;
+    fonte: string;
+    pontos: number;
+}

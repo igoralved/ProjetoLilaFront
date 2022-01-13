ca6a71ad154c41b378cc5f20767eafc8242e3af2
Caroline Moraes <carol.2014pess@gmail.com>
2022-01-13 12:05:23 -0300

    Adicionado service mesa-jogo

---
+++ src/app/service/mesa-jogo.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { Sala } from '../model/sala'
+@Injectable({
+  providedIn: 'root'
+})
+export class MesaJogoService {
+
+constructor() {
+    
+  }
+
+
+}

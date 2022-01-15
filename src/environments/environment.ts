72700e862658201559a0f0c695cafae5dd489f6f
Paola Arrueé <paolaarruee@gmail.com>
2022-01-15 12:23:15 -0300

    Alterando para o 8080

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  API_URL: 'http://localhost:8090/',
+  API_URL: 'http://localhost:8080/',
   CLIENT_URL: 'http://localhost:4200/',
   WEBSOCKET_URL: 'ws://localhost:8080/'
 };

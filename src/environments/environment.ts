2d91fa98a9745b07481efed90e3bfa3e14100a73
anderson fuhr <andersonfuhr@yahoo.com.br>
2022-01-15 21:25:30 -0300

    refatoração do layout e add novo heroku

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  API_URL: 'http://localhost:8080/',
+  API_URL: 'https://backendlila.herokuapp.com/',
   CLIENT_URL: 'http://localhost:4200/',
   WEBSOCKET_URL: 'ws://localhost:8080/'
 };

a4660392247f8871577a2d47df4850e747982a24
anderson fuhr <andersonfuhr@yahoo.com.br>
2022-01-16 00:59:31 -0300

    add novo endereço de deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   API_URL:' https://dev-backendlila.herokuapp.com/',
-  CLIENT_URL: 'https://dev-coracoes-da-lila.netlify.app/',
+  CLIENT_URL: 'https://dev-coracoes-da-lila.herokuapp.com/',
   WEBSOCKET_URL: 'ws://dev-backendlila.herokuapp.com/'
 };

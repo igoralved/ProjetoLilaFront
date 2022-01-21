ee3724a58c4dca5056c090502f3b1a0e168377f4
felipebonadio <febonadio@gmail.com>
2022-01-21 12:06:39 -0300

    Criado o componete de guia rapido e inserido no mesa-jogo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -35,6 +35,7 @@ import { MesaJogoComponent } from './mesa/mesa-jogo/mesa-jogo.component';
 import { AreaJogadoresComponent } from './mesa/area-jogadores/area-jogadores.component';
 import { MesaJogoService } from './service/mesa-jogo.service';
 import { AreaDeCompraComponent } from './area-de-compra/area-de-compra.component';
+import { GuiaRapidoComponent } from './mesa/guia-rapido/guia-rapido.component';
 
 @NgModule({
   declarations: [
@@ -52,7 +53,8 @@ import { AreaDeCompraComponent } from './area-de-compra/area-de-compra.component
     MesaJogoComponent,
     AreaJogadoresComponent,
     MaoJogadorComponent,
-    AreaDeCompraComponent
+    AreaDeCompraComponent,
+    GuiaRapidoComponent
   ],
   imports: [
     BrowserModule,

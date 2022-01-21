7973aa7b9c1ce34c7b26c030d8b85897c58fc273
felipebonadio <febonadio@gmail.com>
2022-01-21 10:58:35 -0300

    preparando metodo de conexao com back

--- src/app/service/mesa-jogo.service.ts
+++ src/app/service/mesa-jogo.service.ts
@@ -1,5 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
 import { Jogador } from '../model/jogador';
 import { Sala } from '../model/sala';
 import { MesaService } from './mesa.service';
@@ -10,7 +12,7 @@ export class MesaJogoService {
   private emitSala$ = new BehaviorSubject<Sala>({} as Sala);
   private emitJogador$ = new BehaviorSubject<Jogador>({} as Jogador);
 
-  constructor(private mesaService: MesaService) {}
+  constructor(private mesaService: MesaService, private http: HttpClient) {}
 
   getemitSalaObservable(): Observable<Sala> {
     return this.emitSala$.asObservable();
@@ -26,5 +28,10 @@ export class MesaJogoService {
 
   getemitJogadorSubject() {
     return this.emitJogador$;
+  }
+
+  //TODO: metodo para chamar do back
+  getDadoJogada(): Observable<Sala>{
+    return this.http.get<Sala>(environment.API_URL + '/')
   }
 }

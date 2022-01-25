0966dfc97ce48f90bbf28503b22f9be8ab7bc02b
AndreArjDev <andre.021arj@gmail.com>
2022-01-25 10:59:14 -0300

    implementação do teste para inicia-partida.service
    Co-authored-by: Gabriel Lima Brito da Silva <gabriellimab@gmail.com>

--- src/app/service/inicia-partida.service.spec.ts
+++ src/app/service/inicia-partida.service.spec.ts
@@ -1,16 +1,51 @@
-import { TestBed } from '@angular/core/testing';
-
+import { TestBed, inject } from '@angular/core/testing';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
 import { IniciaPartidaService } from './inicia-partida.service';
+import { environment } from 'src/environments/environment';
+
 
 describe('IniciaPartidaServiceService', () => {
+  let httpTestingController: HttpTestingController;
   let service: IniciaPartidaService;
+  let hash: string = 'eunaosei'
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(IniciaPartidaService);
+    TestBed.configureTestingModule({
+      imports:[HttpClientTestingModule],
+      providers:[IniciaPartidaService]
+    });
+    httpTestingController = TestBed.get(HttpTestingController);
+    service = TestBed.get(IniciaPartidaService);
+    
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('deve ser capaz de criar um servico', inject([IniciaPartidaService], (service: IniciaPartidaService) => {
+
+    expect(service).toBeDefined();
+  }));
+
+
+describe('get quantidade de jogadores',() => {
+
+  it('deve chamar o get com URL válida',() => {
+    service.getQuantidadeJogadores(hash).subscribe(quantidade => 
+      expect(quantidade).toEqual(1));
+
+      
   });
+    
+    const req = httpTestingController.expectOne(`${environment.API_URL}sala/numeroJogadores/`+
+    hash);
+
+    req.flush(1);
+
+    httpTestingController.verify();
+  })
+
 });
+
+
+
+
+
+

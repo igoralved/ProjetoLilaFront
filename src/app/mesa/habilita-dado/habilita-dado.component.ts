17664ade69d2bd6666e63798f8de2645a68ced70
felipebonadio <febonadio@gmail.com>
2022-01-26 19:18:04 -0300

    arrumado o bug da necessidade de dois cliques e alterando o botao para rodar e resetar o dado

--- src/app/mesa/habilita-dado/habilita-dado.component.ts
+++ src/app/mesa/habilita-dado/habilita-dado.component.ts
@@ -2,45 +2,19 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
 import { Sala } from 'src/app/model/sala';
 import { MesaJogoService } from 'src/app/service/mesa-jogo.service';
-import {
-  trigger,
-  state,
-  style,
-  animate,
-  transition,
-  // ...
-} from '@angular/animations';
-
 
 @Component({
   selector: 'app-habilita-dado',
   templateUrl: './habilita-dado.component.html',
-  styleUrls: ['./habilita-dado.component.scss'],
-  animations: [
-    trigger('openClose', [      
-      state('open', style({        
-        opacity: 1,        
-      })),
-      state('closed', style({
-        opacity: 0,       
-      })),
-      transition('open => closed', [
-        animate('0.5s')
-      ]),
-      transition('closed => open', [
-        animate('1s')
-      ]),
-    ]),
-  ],
+  styleUrls: ['./habilita-dado.component.scss'], 
 })
-
 
 export class HabilitaDadoComponent implements OnInit {
  
   @ViewChild('dice')
   public dado: ElementRef<Element> ={} as ElementRef;
   public sala: Sala = {} as Sala;
-  
+  public numero: number = 0;
   constructor(private mesaJogoService: MesaJogoService) {     
   }
   
@@ -49,23 +23,35 @@ export class HabilitaDadoComponent implements OnInit {
       .getemitSalaObservable()
       .subscribe((sala) => (this.sala = sala));      
   }
-
+   
   rolarDado() {
+    const node = this.dado.nativeElement;      
+    this.numero = this.gerarNumeroAleatorio(1,6);
     //TODO: método para buscar do back
     // this.mesaJogoService.comprarCarta(this.sala).subscribe(sala => {this.sala = sala})
-    // const number = this.sala.dado + '';
-    const node = this.dado.nativeElement;      
-    const number = this.gerarNumeroAleatorio(1,6).toString();
-    console.warn(number)
+    // const number = this.sala.dado + '';    
+    console.warn(this.numero)
     if (node instanceof HTMLElement) {
       this.trocarClasses(node);
-      node.dataset['roll'] = number;
+      node.dataset['roll'] = this.numero.toString();
     }
   }  
+
+  resetarDado(){
+    const node = this.dado.nativeElement;    
+    if (node instanceof HTMLElement) {
+      this.resetarClasse(node);    
+    }
+    this.numero = 0;  
+  }
   
   trocarClasses(die: HTMLElement) {
-    die.classList.toggle('even-roll');    
+   die.classList.add('even-roll');    
   }  
+
+  resetarClasse(die: HTMLElement){
+    die.classList.remove('even-roll')
+  }
 
   gerarNumeroAleatorio(min: number, max: number) {
     min = Math.ceil(min);
